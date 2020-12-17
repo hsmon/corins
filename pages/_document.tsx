6c4e38514bbccfdc07c0755316842b0beb5dfe0f
hsmon <29920723+hso92@users.noreply.github.com>
2020-12-17 21:03:26 +0900

    viewportの移動

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,7 +23,6 @@ export default class MyDocument extends Document<Props> {
         <Head>
           <meta charSet="UTF-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="viewport" content="width=device-width,initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
           {this.props.styleTags}
           <script
