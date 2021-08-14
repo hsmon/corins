a7ff59f1e30acdf923ace4b65d4f3bc00f9d4017
hsmon <hso92.bb@gmail.com>
2021-08-14 11:00:23 +0900

    :ambulance: サーバーサイド周りの修正

--- pages/api/screenshot.ts
+++ pages/api/screenshot.ts
@@ -39,7 +39,7 @@ export type ScreenshotProps = (
   username: string,
   password: string,
   monitorSize: MonitorSizeKey
-) => Promise<Partial<ScreenshotReturnType> | undefined>
+) => Promise<Partial<ScreenshotReturnType>>
 
 export type ScreenshotAllType = ScreenshotInterface | ScreenshotErrorType
 
@@ -62,13 +62,41 @@ const selectMonitorSize: (
 }
 
 const argsOption = [
-  '--disable-gpu',
+  '--autoplay-policy=user-gesture-required',
+  '--disable-background-networking',
+  '--disable-background-timer-throttling',
+  '--disable-backgrounding-occluded-windows',
+  '--disable-breakpad',
+  '--disable-client-side-phishing-detection',
+  '--disable-component-update',
+  '--disable-default-apps',
   '--disable-dev-shm-usage',
+  '--disable-domain-reliability',
+  '--disable-extensions',
+  '--disable-features=AudioServiceOutOfProcess',
+  '--disable-hang-monitor',
+  '--disable-ipc-flooding-protection',
+  '--disable-notifications',
+  '--disable-offer-store-unmasked-wallet-cards',
+  '--disable-popup-blocking',
+  '--disable-print-preview',
+  '--disable-prompt-on-repost',
+  '--disable-renderer-backgrounding',
   '--disable-setuid-sandbox',
+  '--disable-speech-api',
+  '--disable-sync',
+  '--hide-scrollbars',
+  '--ignore-gpu-blacklist',
+  '--metrics-recording-only',
+  '--mute-audio',
+  '--no-default-browser-check',
   '--no-first-run',
+  '--no-pings',
   '--no-sandbox',
   '--no-zygote',
-  '--single-process'
+  '--password-store=basic',
+  '--use-gl=swiftshader',
+  '--use-mock-keychain'
 ]
 
 const screenshot: ScreenshotProps = async (
@@ -96,9 +124,9 @@ const screenshot: ScreenshotProps = async (
           headless: chrome.headless
         })
     const page = await browser.newPage()
-    await page.emulateMediaType('screen')
 
     await Promise.allSettled([
+      page.emulateMediaType('screen'),
       page.authenticate({
         username,
         password
